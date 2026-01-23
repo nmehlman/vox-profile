88d859acdf87eba3631b6b4a982e39e81b122092
Nick Mehlman <nmehlman@usc.edu>
2026-01-23 20:17:43 +0000

    Fix device

--- src/model/emotion/whisper_emotion.py
+++ src/model/emotion/whisper_emotion.py
@@ -244,7 +244,7 @@ class WhisperWrapper(
                 sampling_rate=16000,
                 max_length=max_audio_len
             )
-            features = features.input_features.cuda()
+            features = features.input_features.to(x.device)
         else:
             max_audio_len = 15*16000
             features = self.feature_extractor(
@@ -253,7 +253,7 @@ class WhisperWrapper(
                 sampling_rate=16000,
                 max_length=max_audio_len
             )
-            features = features.input_features.cuda()
+            features = features.input_features.to(x.device)
         
         # 2. get length and mask
         if length is not None:
